eba2bfe291741b97811ab7a7d6a16522e2c37ad6
Vamsi Akkala <vamsiakkala@Mac.bbrouter>
2026-04-12 01:35:59 +0530

    New

---
+++ vars/todo.groovy
@@ -0,0 +1,48 @@
+def call(Map params = [:]) {
+  // Start Default Arguments
+  def args = [
+          NEXUS_IP               : '172.31.14.124',
+  ]
+  args << params
+
+  // End Default + Required Arguments
+
+  pipeline {
+     agent {
+
+      //label "NODES"
+      label "${args.Slave}"
+
+     }
+
+     environment {
+
+      Service    = "${args.Service}"
+      Slave      = "${args.Slave}"
+      App_type   = "${args.App_type}"
+
+     }
+     stages {
+        stage ('make artificats - Nginx') {
+           when {
+          
+           environment name: 'App_type', value: 'Nginx'
+
+           }
+         steps {
+            sh '''
+              zip -r ${Service}.zip *
+             '''
+            }
+        }
+        stage ('upload the Artifact to Nexus') {
+           steps {
+            sh'''
+               curl -v -u admin:nexus123 --upload-file frontend.zip  http://13.222.207.100:8081/repository/frontend/frontend.zip
+
+             '''
+            }
+        }   
+      }
+    }
+}
