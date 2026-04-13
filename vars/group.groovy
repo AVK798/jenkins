63a77f20293160ede032f9f5b837c67f2def3b07
Vamsi Akkala <vamsiakkala@Mac.bbrouter>
2026-04-14 04:13:15 +0530

    New

--- vars/group.groovy
+++ vars/group.groovy
@@ -18,13 +18,26 @@ def Make_artifacts(App_type, Service) {
     }
 }
 
+// def upload_artifacts(Service) {
+//   get_branch = "env | grep GIT_BRANCH | awk -F / '{print \$NF}' | xargs echo -n"
+//   def get_branch_exec=sh(returnStdout: true, script: get_branch)
+//   def FILENAME=Service+'-'+get_branch_exec+'.zip'
+//    command = "curl -f -v -u admin:nexus123 --upload-file ${FILENAME} http://172.31.72.40:8081/repository/${Service}/${FILENAME}"
+//    def execute_state=sh(returnStdout: true, script: command)
+//   //manager.addShortText("deployed")
+// }
+
 def upload_artifacts(Service) {
-  get_branch = "env | grep GIT_BRANCH | awk -F / '{print \$NF}' | xargs echo -n"
-  def get_branch_exec=sh(returnStdout: true, script: get_branch)
-  def FILENAME=Service+'-'+get_branch_exec+'.zip'
-   command = "curl -f -v -u admin:nexus123 --upload-file ${FILENAME} http://172.31.72.40:8081/repository/${Service}/${FILENAME}"
-   def execute_state=sh(returnStdout: true, script: command)
-  //manager.addShortText("deployed")
+    def name = ""
+    if (env.TAG_NAME) {
+        name = env.TAG_NAME
+    } else {
+        name = env.BRANCH_NAME ?: "main"
+    }
+    def FILENAME = "${Service}-${name}.zip"
+    echo "Artifact Name: ${FILENAME}"
+    command = "curl -f -v -u admin:nexus123 --upload-file ${FILENAME} http://172.31.72.40:8081/repository/${Service}/${FILENAME}"
+    def execute_state=sh(returnStdout: true, script: command)
 }
 
 def Code_builds(App_type, Service) {
