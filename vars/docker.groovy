641b04b95bc6d538dd6f6b5c7f68da6d3aeedfba
AVK798 <vmasiakkala.9549@gmail.com>
2026-06-02 00:54:09 +0530

    New

--- vars/docker.groovy
+++ vars/docker.groovy
@@ -127,8 +127,17 @@ def call(Map params = [:]) {
                   groups
                   ls -l /var/run/docker.sock
                   docker ps
-                  docker build -t local .
+                  aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 400138960249.dkr.ecr.us-east-1.amazonaws.com
+                  docker build -t 400138960249.dkr.ecr.us-east-1.amazonaws.com/${Service}:latest .
 
+                '''
+             }
+        }
+        stage ('Docker Push') {
+          steps {
+                sh '''
+                  
+                 docker push 400138960249.dkr.ecr.us-east-1.amazonaws.com/${Servcie}:latest
                 '''
              }
         }
