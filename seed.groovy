c8c0875e99f5a495e00abd70a42b3342ae0e01f1
AVK798 <vmasiakkala.9549@gmail.com>
2026-06-09 01:22:16 +0530

    New

--- seed.groovy
+++ seed.groovy
@@ -23,3 +23,25 @@ folder('Todo-App')
         }
     }
 }
+
+folder('Todo-App/K8S')
+
+['frontend','login','todo','users'].each { app ->
+    pipelineJob("Todo-App/${app}") {
+        definition {
+            cpsScm {
+                scm {
+                    git {
+                        remote {
+                            url("https://github.com/AVK798/${app}.git")
+                            refspec('+refs/heads/*:refs/remotes/origin/* +refs/tags/*:refs/tags/*')
+                        }
+                        branches('*/main', 'refs/tags/*')
+                    }
+                }
+                scriptPath('Jenkinsfile-K8S')
+            }
+        }
+
+    }
+}
