472ea0674afa03aa3cf37e62ac44963b5c8c1514
AVK798 <vmasiakkala.9549@gmail.com>
2026-06-09 01:44:20 +0530

    New

--- seed.groovy
+++ seed.groovy
@@ -1,27 +1,27 @@
-folder('TodoApp')
-['frontend','login','todo','users'].each { app ->
-    pipelineJob("TodoApp/${app}") {
-        definition {
-            cpsScm {
-                scm {
-                    git {
-                        remote {
-                            url("https://github.com/AVK798/${app}.git")
-                            refspec('+refs/heads/*:refs/remotes/origin/* +refs/tags/*:refs/tags/*')
-                        }
-                        branches('*/main', 'refs/tags/*')
-                    }
-                }
-                scriptPath('Jenkinsfile-Docker')
-            }
-        }
+// folder('TodoApp')
+// ['frontend','login','todo','users'].each { app ->
+//     pipelineJob("TodoApp/${app}") {
+//         definition {
+//             cpsScm {
+//                 scm {
+//                     git {
+//                         remote {
+//                             url("https://github.com/AVK798/${app}.git")
+//                             refspec('+refs/heads/*:refs/remotes/origin/* +refs/tags/*:refs/tags/*')
+//                         }
+//                         branches('*/main', 'refs/tags/*')
+//                     }
+//                 }
+//                 scriptPath('Jenkinsfile-Docker')
+//             }
+//         }
 
-        triggers {
-            githubPush()              // webhook trigger
-            scm('H/2 * * * *')        // poll every 2 minutes
-        }
-    }
-}
+//         triggers {
+//             githubPush()              // webhook trigger
+//             scm('H/2 * * * *')        // poll every 2 minutes
+//         }
+//     }
+// }
 folder('TodoApp-K8S')
 ['frontend','login','todo','users'].each { app ->
     pipelineJob("TodoApp-K8S/${app}") {
